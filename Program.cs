17bb9a195538830bfde92988f9f1d38934348519
TylerRoss212 <Tyler_Ross3@alumni.baylor.edu>
2024-03-03 11:19:04 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using blog_frontend.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // Allow the app to be access outside the docker container
+    //app.Urls.Add("http://0.0.0.0:8080");
+
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
